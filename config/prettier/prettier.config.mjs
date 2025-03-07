da27ee266c63090d2e449b7683d3be65143ed1b1
Rafael Drigo <rafael.drigo@superior-ind.com>
2025-03-07 15:13:41 -0300

    feat: Creation of the configuration packages for Prettier and ESLint

---
+++ config/prettier/prettier.config.mjs
@@ -0,0 +1,15 @@
+/** @typedef {import('prettier').Config} PrettierConfig */
+
+/** @type { PrettierConfig } */
+export default {
+  printWidth: 120,
+  endOfLine: 'lf',
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: true,
+  requirePragma: false,
+  bracketSpacing: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+};
